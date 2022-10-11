2d2c5ca4e487e1e363fa2cd80decdcef7c90b0bb
Charlie Moran <Carlosamoran18@gmail.com>
2022-10-11 13:59:17 -0400

    freshness

--- food.js
+++ food.js
@@ -4,24 +4,21 @@ class Food {
       this.daysToSpoil = daysToSpoil;
       this.fresh = fresh;
     }
+
+    fresh() {
+        this.daysToSpoil < 1 ? (this.fresh = false) : (this.fresh = true);
+        this.daysToSpoil > 0
+          ? console.log(
+              `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+            )
+          : console.log(`${this.name} has spoiled.`);
+      }
+    
   
     prepare() {
       console.log(`${this.name} is being prepared`);
     }
   
-    fresh() {
-      this.daysToSpoil < 1 ? (this.fresh = false) : (this.fresh = true);
-      this.daysToSpoil > 0
-        ? console.log(
-            `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
-          )
-        : console.log(`${this.name} has spoiled.`);
-    }
-  
-    oneDayPassed() {
-      this.daysToSpoil -= 1;
-      this.fresh();
-    }
 }  
 // Do not edit below this line
 module.exports = Food;
